68a0a9f367a9d75e677f9896f4bd371b31e6be56
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-12-01 22:55:35 +0700

    observer

---
+++ src/BinObserver.java
@@ -0,0 +1,11 @@
+public class BinObserver extends Observer{
+    public BinObserver(Subject subject) {
+        this.subject = subject;
+        this.subject.add(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.print(" " + Integer.toBinaryString(subject.getState()));
+    }
+}
